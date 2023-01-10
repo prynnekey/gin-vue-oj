cb6414148c6cb9cc3f467fea4d2dc1054d54a6c4
prynnekey <prynnekey@gmail.com>
2023-01-10 16:57:37 +0800

    新增:常量

--- define/define.go
+++ define/define.go
@@ -3,4 +3,10 @@ package define
 var (
 	PROBLEM_GET_PAGE      = "1"
 	PROBLEM_GET_PAGE_SIZE = "20"
+
+	// 用户注册时生成验证码的长度
+	EMAIL_CODE_LENGTH = 6
+
+	// 将验证码储存到Redis的格式
+	REDIS_SAVE_EMAIL_CODE = "user:email_code:"
 )
