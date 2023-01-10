55f0866456f59c3850783d9b105d793887b1b84a
prynnekey <prynnekey@gmail.com>
2023-01-10 16:58:12 +0800

    新增:redis配置

--- models/init.go
+++ models/init.go
@@ -1,12 +1,14 @@
 package models
 
 import (
+	"github.com/go-redis/redis/v9"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
 var DB = Init()
+var REDIS = initRedis()
 
 func Init() *gorm.DB {
 	dsn := "root:prynnekey@tcp(127.0.0.1:3306)/gin-vue-oj?charset=utf8mb4&parseTime=True&loc=Local"
@@ -19,4 +21,14 @@ func Init() *gorm.DB {
 	}
 
 	return db
+}
+
+func initRedis() *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "192.168.138.128:6379",
+		Password: "123456", // no password set
+		DB:       0,        // use default DB
+	})
+
+	return rdb
 }
